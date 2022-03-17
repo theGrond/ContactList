49145ca0e010940f5b7a27fea66838c2c0a52393
“gohlke_max” <gohlke.max@gmx.de>
2022-03-17 11:11:45 +0700

    chnge

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var history = require('connect-history-api-fallback');
 
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-app = express()
-//add this middleware
-app.use(history());    
-app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started '+ port) 
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(history()); 
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 3000;
+app.listen(port);
