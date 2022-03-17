1d1691773cdec0db74cee7937deb0953303cc25e
“gohlke_max” <gohlke.max@gmx.de>
2022-03-17 11:27:39 +0700

    remove contact button hover

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,7 +8,7 @@
     <!-- This creates a logo from vue on the left side of the header -->
     <div class="left">
       <img class="logo" src="../assets/logo.png" />
-      <div class="link">
+      <div class="contact">
         <a>Contacts</a>
       </div>
     </div>
@@ -59,6 +59,17 @@ export default {
 .right {
   margin-left: auto;
   padding-right: 20px;
+}
+
+.contact {
+  color: darkgrey;
+  border-left: solid 1px rgb(53, 53, 53);
+  border-right: solid 1px rgb(53, 53, 53);
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+  padding-right: 10px;
 }
 
 .link {
